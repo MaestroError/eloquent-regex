c70b1d25a7eaa0a782dadddc078a8138fcb087d3
maestroerror <revaz.gh@gmail.com>
2024-04-09 21:14:11 +0400

    Updated URL and date patterns, updated Docs, created Assistant GPT

--- src/Patterns/DatePattern.php
+++ src/Patterns/DatePattern.php
@@ -9,7 +9,7 @@ class DatePattern extends BasePattern {
 
     use Pattern;
 
-    protected string $pattern = "(\\d{4}([-\/.])\\d{2}\\2\\d{2})|(\\d{2}([-\/.])\\d{2}\\4\\d{2,4})";
+    protected string $pattern = "(\\d{2,4}([-\/.])\\d{2}([-\/.])\\d{2,4})";
 
     public static string $name = "date";
 
